e2f0c38e7f24c8b1c7080186beb4af483e8ae3a5
NargizNargiz <nargiz1397@mail.ru>
2018-10-06 19:51:48 +0300

    less4

--- lecture04/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/binaryUtil.kt
+++ lecture04/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/practice/binaryUtil.kt
@@ -1,9 +1,18 @@
 package io.rybalkinsd.kotlinbootcamp.practice
 
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
 /**
  * "8".toBinary() == "1000"
  *
  * @throws NumberFormatException if could not be parsed as Number
  * @throws IllegalArgumentException if target string is null
  */
-fun String?.toBinary(): String = TODO()
+fun exThrower(){
+    throw NumberFormatException("wrong format")
+}
+fun String?.toBinary(): String = when(this) {
+    null ->   throw IllegalArgumentException()
+    else -> toBigInteger(2).toString(2)
+}
