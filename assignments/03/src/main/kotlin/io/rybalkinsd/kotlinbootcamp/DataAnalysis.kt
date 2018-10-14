2cb8513630e389cc61ad7c0457177db6f3ce3eef
NargizNargiz <nargiz1397@mail.ru>
2018-10-14 16:17:49 +0300

    HW3 bad effort

--- assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
+++ assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
@@ -1,6 +1,22 @@
 package io.rybalkinsd.kotlinbootcamp
 
-class RawProfile(val rawData: String)
+class RawProfile(val rawData: String?) {
+    fun getList(): List<String?> = rawData?.filter { ch -> ch != '\n' }!!. split(',')
+    fun getMap(): MutableMap<String?, String?> {
+        var map: MutableMap<String?, String?> = getList().map { el -> el?.split('=', limit = 2) }. associate { it!![0] to it[1].toLowerCase() }. toMutableMap()
+        map.getOrPut("firstName") { null }
+        map.getOrPut("lastName") { null }
+        if (map.containsKey("age")) {
+            if (!(map["age"]!!.fold(true) { acc, el -> el.isDigit() and acc }))
+                map["age"] = null
+        } else
+            map.plus(Pair("age", null))
+        return map
+    }
+}
+class FacebookProfile(id: Long = 0, firstName: String? = null, lastName: String? = null, age: Int? = null) : Profile(id = id, firstName = firstName, lastName = lastName, age = age, dataSource = DataSource.FACEBOOK)
+class LinkedinProfile(id: Long = 0, firstName: String? = null, lastName: String? = null, age: Int? = null) : Profile(id = id, firstName = firstName, lastName = lastName, age = age, dataSource = DataSource.LINKEDIN)
+class VkProfile(id: Long = 0, firstName: String? = null, lastName: String? = null, age: Int? = null) : Profile(id = id, firstName = firstName, lastName = lastName, age = age, dataSource = DataSource.VK)
 
 enum class DataSource {
     FACEBOOK,
@@ -15,42 +31,38 @@ sealed class Profile(
     var age: Int? = null,
     var dataSource: DataSource
 )
+class evalProfileNew(id: Long, firstName: String? = null, lastName: String? = null, age: Int? = null, dataSource: DataSource) : Profile(id = id, firstName = firstName, lastName = lastName, age = age, dataSource = dataSource) {
+    fun choiceSource() = when (dataSource) {
+        DataSource.VK -> VkProfile(id = id, firstName = firstName, lastName = lastName, age = age)
+        DataSource.FACEBOOK -> FacebookProfile(id = id, firstName = firstName, lastName = lastName, age = age)
+        else -> LinkedinProfile(id = id, firstName = firstName, lastName = lastName, age = age)
+    }
+}
 
-/**
- * Task #1
- * Declare classes for all data sources
- */
-class FacebookProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id = id)
+fun String.returnSocial(): DataSource = when (this.toLowerCase()) {
+    "vk" -> DataSource.VK
+    "facebook" -> DataSource.FACEBOOK
+    else -> DataSource.LINKEDIN
+}
+fun equalProfiles(p1: Profile, p2: Profile) = (p1.firstName == p2.firstName) and (p1.lastName == p2.lastName) and (p1.age == p2.age)
 
-/**
- * Task #2
- * Find the average age for each datasource (for profiles that has age)
- *
- * TODO
- */
-val avgAge: Map<DataSource, Double> = emptyMap()
+val avgAge: Map<DataSource, Double> = parsData().groupBy { it.dataSource }
+        .toMutableMap()
+        .map { it }
+        .associate { it.key to it.value.map { it.age }
+                .filter { it != null }.map { it!!.toInt() }. average() }
 
-/**
- * Task #3
- * Group all user ids together with all profiles of this user.
- * We can assume users equality by : firstName & lastName & age
- *
- * TODO
- */
-val groupedProfiles: Map<Long, List<Profile>> = emptyMap()
+val groupedProfiles: Map<Long, List<Profile>> = parsData().groupBy { it.id }
 
-/**
- * Here are Raw profiles to analyse
- */
 val rawProfiles = listOf(
     RawProfile("""
-            fisrtName=alice,
+            firstName=alice,
             age=16,
             source=facebook
             """.trimIndent()
     ),
     RawProfile("""
-            fisrtName=Dent,
+            firstName=Dent,
             lastName=kent,
             age=88,
             source=linkedin
@@ -63,13 +75,13 @@ val rawProfiles = listOf(
             """.trimIndent()
     ),
     RawProfile("""
-            fisrtName=bella,
+            firstName=bella,
             age=36,
             source=vk
             """.trimIndent()
     ),
     RawProfile("""
-            fisrtName=angel,
+            firstName=angel,
             age=I will not tell you =),
             source=facebook
             """.trimIndent()
@@ -78,12 +90,12 @@ val rawProfiles = listOf(
     RawProfile(
         """
             lastName=carol,
-            source=vk
-            age=49,
+            source=vk,
+            age=49
             """.trimIndent()
     ),
     RawProfile("""
-            fisrtName=bob,
+            firstName=bob,
             lastName=John,
             age=47,
             source=linkedin
@@ -91,9 +103,47 @@ val rawProfiles = listOf(
     ),
     RawProfile("""
             lastName=kent,
-            fisrtName=dent,
+            firstName=dent,
             age=88,
             source=facebook
             """.trimIndent()
     )
 )
+fun parsData(): MutableList<Profile> {
+    val listParsData = rawProfiles.map { it.getMap() }
+    var person: Profile
+    var listProfiles: MutableList<Profile> = mutableListOf()
+    for (i in listParsData.indices) {
+        person = evalProfileNew(id = i.toLong(),
+                dataSource = listParsData[i]["source"]!!.returnSocial(),
+                firstName = listParsData[i]["firstName"],
+                lastName = listParsData[i]["lastName"],
+                age = listParsData[i]["age"]?.toInt()).choiceSource()
+        var listId = listProfiles.map { elem -> (if (equalProfiles(person, elem)) elem.id else -1L) }.filter { it != -1L }
+        if (!listId.isEmpty())
+            person = evalProfileNew(id = listId[0], dataSource = person.dataSource, firstName = person.lastName, lastName = person.lastName, age = person.age).choiceSource()
+        println(person)
+        listProfiles.add(person)
+    }
+//    var listToAvg: MutableMap<DataSource, List<Profile>> = listProfiles.groupBy { it.dataSource }.toMutableMap()
+    return listProfiles
+}
+fun main(args: Array<String>) {
+    val listParsData = rawProfiles.map { it.getMap() }
+    var person: Profile
+    var listProfiles: MutableList<Profile> = mutableListOf()
+    for (i in listParsData.indices) {
+        person = evalProfileNew(id = i.toLong(),
+                dataSource = listParsData[i]["source"]!!.returnSocial(),
+                firstName = listParsData[i]["firstName"],
+                lastName = listParsData[i]["lastName"],
+                age = listParsData[i]["age"]?.toInt()).choiceSource()
+        var listId = listProfiles.map { elem -> (if (equalProfiles(person, elem)) elem.id else -1L) }.filter { it != -1L }
+        if (!listId.isEmpty())
+            person = evalProfileNew(id = listId[0], dataSource = person.dataSource, firstName = person.lastName, lastName = person.lastName, age = person.age).choiceSource()
+        println(person)
+        listProfiles.add(person)
+    }
+}
+//    avgAge = listToAvg.map { it }.associate { it.key to it.value.map { it.age }.filter { it != null }.map { it!!.toInt() }.average() }
+//    groupedProfiles = listProfiles.groupBy { it.id }
